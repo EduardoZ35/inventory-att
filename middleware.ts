b8033e9e5d47031b216cf064427d655d99f1d404
EduardoZ35 <eduardo.zuniga@edu.udla.cl>
2025-08-19 12:29:01 -0400

    admin_usuarios_fix2

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,27 @@
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { req, res }
+    {
+      cookies: {
+        get(name: string) {
+          return req.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          res.cookies.set(name, value, options)
+        },
+        delete(name: string) {
+          res.cookies.delete(name)
+        },
+      },
+    }
   )
 
   await supabase.auth.getSession()
